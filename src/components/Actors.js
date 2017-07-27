d4939bd6845a690fc013e20e853110b7904cd1c1
christopher-hague <christopher.a.hague@gmail.com>
2017-07-27 15:43:44 -0500

    first

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const actorInfo = actors.map(actor =>
+    <div>
+      <h3>{actor.name}</h3>
+      {actor.movies.map(movie => <ul>{movie}</ul>)}
+    </div>
+  )
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorInfo}
     </div>
   );
 };
